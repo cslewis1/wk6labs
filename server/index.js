0ea14f035e4eff5d5ec41d8f814096c382c25ce9
Charneaka Lewis <clewis20@ggc.edu>
2023-05-11 17:40:26 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 const { getCompliment, getFortune, getImage, getUserList, createQuote, deleteQuote, updateRating } = require("./controller");
 
